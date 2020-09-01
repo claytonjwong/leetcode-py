015026a34d8ed9eb78568fb274f8f21c538b4dd1
Clayton Wong <claytonjwong@gmail.com>
2020-09-01 06:35:37 -0700

    1567. Maximum Length of Subarray With Positive Product (cnt -> even)

--- 1567_max_len_subarray_with_positive_prod.py
+++ 1567_max_len_subarray_with_positive_prod.py
@@ -5,8 +5,10 @@
 # A: https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/discuss/822464/Javascript-Python3-C%2B%2B-Sliding-Window
 #
 
+from typing import List
+
 class Solution:
-    def getMaxLen(self, A: List[int], cnt = 0, best = 0) -> int:
+    def getMaxLen(self, A: List[int], even = 1, best = 0) -> int:
         A.append(0)  # ⭐️ sentinel value
         N = len(A)
         i = 0
@@ -15,12 +17,16 @@ class Solution:
             # case 1: ➖ collapse window [i 👉 ..j]
             while j < N and not A[j]:
                 while i < j:
-                    cnt = cnt - 1 if A[i] < 0 else cnt; i += 1
-                    best = best if cnt & 1 else max(best, j - i)
+                    if A[i] < 0: even ^= 1
+                    i += 1
+                    if even: best = max(best, j - i)
                 i = j + 1
                 j = j + 1
             # case 2: ➕ expand window [i..j 👉 ]
             while j < N and A[j]:
-                cnt = cnt + 1 if A[j] < 0 else cnt; j += 1
-                best = best if cnt & 1 else max(best, j - i)
+                if A[j] < 0: even ^= 1
+                j += 1
+                if even: best = max(best, j - i)
         return best
+
+Solution().getMaxLen([1,2,-3,-4])
