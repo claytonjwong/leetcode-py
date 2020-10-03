72e2b981f739218fbebb9f046f6cd01d8aea9c84
Clayton Wong <claytonjwong@gmail.com>
2020-10-03 14:39:11 -0700

    1602. Find Nearest Right Node in Binary Tree (DFS)

--- 1602_nearest_right_neighbor.py
+++ 1602_nearest_right_neighbor.py
@@ -2,7 +2,7 @@
 # 1602. Find Nearest Right Node in Binary Tree
 #
 # Q: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
-# A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-BFS-solutions
+# A: https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/875464/Javascript-Python3-C%2B%2B-DFS-and-BFS
 #
 
 from collections import deque
@@ -13,6 +13,23 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# DFS
+class Solution:
+    def findNearestRightNode(self, root: TreeNode, T: TreeNode, found = -1) -> TreeNode:
+        def go(node = root, depth = 0):
+            nonlocal found
+            if not node:
+                return None
+            if depth == found:
+                return node
+            if node == T:
+                found = depth
+            L = go(node.left,  depth + 1)
+            R = go(node.right, depth + 1)
+            return L if L else R if R else None
+        return go()
+
+# BFS
 class Solution:
     def findNearestRightNode(self, root: TreeNode, T: TreeNode) -> TreeNode:
         q = deque([ root ])
