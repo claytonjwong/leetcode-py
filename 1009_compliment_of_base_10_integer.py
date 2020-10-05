0091421dd3f7eebf111e546af397cb8b363de8ea
Clayton Wong <claytonjwong@gmail.com>
2020-10-05 07:10:48 -0700

    1009. Complement of Base 10 Integer

--- 1009_compliment_of_base_10_integer.py
+++ 1009_compliment_of_base_10_integer.py
@@ -2,17 +2,17 @@
 # 1009. Complement of Base 10 Integer
 #
 # Q: https://leetcode.com/problems/complement-of-base-10-integer/
-# A: https://leetcode.com/problems/complement-of-base-10-integer/discuss/613061/Javascript-and-C%2B%2B-solutions
+# A: https://leetcode.com/problems/complement-of-base-10-integer/discuss/613061/Javascript-Python3-C%2B%2B
 #
 
 # 1-liner
 class Solution:
-    def bitwiseComplement(self, N: int) -> int:
-        return int(''.join(list(map(lambda bit: '1' if bit == '0' else '0',[int(bit) for bit in bin(N)[2:]]))), 2)
+    def bitwiseComplement(self, x: int) -> int:
+        return int(''.join(['1' if bit == '0' else '0' for bit in bin(x)[2:]]), 2)
 
 # verbose
 class Solution:
     def bitwiseComplement(self, x: int, i = 1) -> int:
         while i < x:
-            i <<= 1;
+            i <<= 1
         return 1 if not x else ~x & (i - 1)
