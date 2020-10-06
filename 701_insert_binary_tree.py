32010cddac75309cd8dcd5b250bd01754c29ee7e
Clayton Wong <claytonjwong@gmail.com>
2020-10-06 07:03:18 -0700

    701. Insert into a Binary Search Tree (add concise solution)

--- 701_insert_binary_tree.py
+++ 701_insert_binary_tree.py
@@ -11,6 +11,18 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# concise
+class Solution:
+    def insertIntoBST(self, root: TreeNode, x: int) -> TreeNode:
+        def go(root, x):
+            if x < root.val:
+                root.left =  go(root.left, x)  if root.left  else TreeNode(x)
+            else:
+                root.right = go(root.right, x) if root.right else TreeNode(x)
+            return root
+        return go(root, x) if root else TreeNode(x)
+
+# verbose
 class Solution:
     def insertIntoBST(self, root: TreeNode, x: int) -> TreeNode:
         def go(root, x):
