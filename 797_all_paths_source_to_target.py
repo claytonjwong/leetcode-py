fdee149e22594ebad4b1dbcf7c1d4f09cb3ea66a
Clayton Wong <claytonjwong@gmail.com>
2020-12-10 15:31:24 -0700

    797. All Paths From Source to Target

--- 797_all_paths_source_to_target.py
+++ 797_all_paths_source_to_target.py
@@ -2,19 +2,22 @@
 # 797. All Paths From Source to Target
 #
 # Q: https://leetcode.com/problems/all-paths-from-source-to-target/
-# A: https://leetcode.com/problems/all-paths-from-source-to-target/discuss/752917/Javascript-Python3-C%2B%2B-DFS-%2B-BT
+# A: https://leetcode.com/problems/all-paths-from-source-to-target/discuss/752917/Kt-Js-Py3-Cpp-DFS-%2B-BT
 #
+
+from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, adj: List[List[int]]) -> List[List[int]]:
         paths = []
         N = len(adj)
-        s, t = 0, N - 1
-        def go(u = s, path = [ s ]):
+        s = 0
+        t = N - 1
+        def go(path):
+            u = path[-1]
             if u == t:
-                paths.append(path[:]) # 🎯 target t reached
+                paths.append(path[:])            # 🎯 target t reached
             else:
-                for v in adj[u]:
-                    go(v, path + [v]) # 🚀 explore edge u -> v with implicit ✅ 👀 forward-tracking + 🚫 👀 back-tracking
-        go()
+                for v in adj[u]: go(path + [v])  # implicit ✅ 👀 forward-tracking + 🚫 👀 back-tracking
+        go([s])
         return paths
