160b7745d20a2e8f0ce614bd38a33f572058d795
Clayton Wong <claytonjwong@gmail.com>
2020-12-18 14:46:07 -0700

    334. Increasing Triplet Subsequence (concise)

--- 334_increasing_triplet_subsequence.py
+++ 334_increasing_triplet_subsequence.py
@@ -2,18 +2,15 @@
 # 334. Increasing Triplet Subsequence
 #
 # Q: https://leetcode.com/problems/increasing-triplet-subsequence/
-# A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Track-Two-Minimums
+# A: https://leetcode.com/problems/increasing-triplet-subsequence/discuss/977048/Kt-Js-Py3-Cpp-Construct-a-b-c
 #
 
 from typing import List
 
 class Solution:
-    def increasingTriplet(self, A: List[int], a = float('inf'), b = float('inf')) -> bool:
+    def increasingTriplet(self, A: List[int], a = float('inf'), b = float('inf'), c = float('inf')) -> bool:
         for x in A:
-            if x <= a:
-                a = x
-            elif x <= b:
-                b = x
-            else:
-                return True
-        return False
+            if   x <= a: a = x
+            elif x <= b: b = x
+            elif x <= c: c = x
+        return c < float('inf')
