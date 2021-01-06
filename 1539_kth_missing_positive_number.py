ee35ecc962f843b60f8bbd40f6107466757f4ae9
Clayton Wong <claytonjwong@gmail.com>
2021-01-06 08:28:40 -0700

    1539. Kth Missing Positive Number

--- 1539_kth_missing_positive_number.py
+++ 1539_kth_missing_positive_number.py
@@ -2,11 +2,12 @@
 # 1539. Kth Missing Positive Number
 #
 # Q: https://leetcode.com/problems/kth-missing-positive-number/
-# A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Javascript-Python3-C%2B%2B-Seen-values
+# A: https://leetcode.com/problems/kth-missing-positive-number/discuss/780016/Kt-Js-Py3-Cpp-Seen-Set-%2B-Concise
 #
 
 from typing import List
 
+# seen
 class Solution:
     def findKthPositive(self, A: List[int], K: int) -> int:
         seen = set(A)
@@ -17,3 +18,16 @@ class Solution:
                 if K == 0:
                     return i
             i += 1
+
+# concise
+class Solution:
+    def findKthPositive(self, A: List[int], k: int) -> int:
+        i = 0
+        x = 0
+        while k and i < len(A):
+            x += 1
+            if x == A[i]:
+                i += 1
+            else:
+                k -= 1
+        return x + k
